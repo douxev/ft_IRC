b787ab4aa478e6b122174ebb1998e40a18b4dc68
Maelle <jdoukhan@students.42.fr>
2024-05-28 15:16:19 +0200

    Channel class finished for now

--- srcs/utils.cpp
+++ srcs/utils.cpp
@@ -6,7 +6,6 @@ void	ft_send(int socketfd, const void *c_str, size_t len) {
 	size_t sent = 0;
 
 	while (sent < len) {
-		
 		if (send(socketfd, c_str + sent, len, 0) < 0) {
 			throw SendErrorException();
 		}
