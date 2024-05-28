45765ccad3204edab910fe84fc72c17e69573578
Maelle <jdoukhan@students.42.fr>
2024-05-28 15:19:33 +0200

    fixed ft_send

--- srcs/utils.cpp
+++ srcs/utils.cpp
@@ -4,10 +4,13 @@
 void	ft_send(int socketfd, const void *c_str, size_t len) {
 
 	size_t sent = 0;
+	size_t send_value = 0;
 
 	while (sent < len) {
-		if (send(socketfd, c_str + sent, len, 0) < 0) {
+		send_value = send(socketfd, c_str + sent, len, 0);
+		if (send_value < 0)
 			throw SendErrorException();
-		}
+		else
+			sent += send_value;
 	}
 }
