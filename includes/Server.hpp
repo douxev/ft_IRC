c85447eea6ce7ee64ad27071ea0a783e70e427a7
Roman ASTIE <roman.astie@wanadoo.fr>
2024-05-30 18:24:39 +0200

    changement gestion client connexion/deconnexion

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -17,7 +17,7 @@ public:
 
 	std::string get_motd( void );
 	void	new_user( std::string name, int socketfd, int ip_adress );
-	void	user_quit( const User& name );
+	void	user_quit( const User* name );
 	int		init_server( int ac, char **av );
 	void	manage_loop( void );
 	bool	nick_already_taken( std::string name ) const;
